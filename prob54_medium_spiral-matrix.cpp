94d0fc5e6bb5add17e0681b563b8a5e1c859fbb1
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-02-06 00:50:10 +0800

    20200205

--- prob54_medium_spiral-matrix.cpp
+++ prob54_medium_spiral-matrix.cpp
@@ -82,15 +82,39 @@ private:
 };
 
 // dx = {0, 1, 0, -1}
-class Solution {
+// dy = {1, 0, -1, 0}
+class Solution_2 {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        // 特判
+        if(matrix.empty()) return vector<int>();
+        // dx, dy 的模板写法
         int m = matrix.size();
-        if(m == 0) return vector<int>();
-        return _spiralOrder(matrix, 0, 0);
+        int n = matrix[0].size();
+        int dx[4] = {0, 1, 0, -1};
+        int dy[4] = {1, 0, -1, 0};
+        int x = 0, y = 0, d = 0; // 初始位置 (0, 0), 初始方向(0, 1)
+        vector<vector<bool> > watched(m, vector<bool>(n, false));
+        vector<int> result;
+        for(int i = 0; i < m * n; ++i)
+        {
+            // 一共输出 m * n 个数, 所以做 m * n 次循环
+            // 先判断下一步要不要改方向
+            // 按照原方向，下一个位置点
+            int x_next = x + dx[d];
+            int y_next = y + dy[d];
+            if(x_next == m || y_next == n || x_next < 0 || y_next < 0 || watched[x_next][y_next])
+            {
+                // (x + dx, y + dy) 越界或者此前已经遍历过，则换方向
+                d = (d + 1) % 4;
+            }
+            result.push_back(matrix[x][y]);
+            watched[x][y] = true; // (x, y) 已经遍历过
+            x += dx[d];
+            y += dy[d];
+        }
+        return result;
     }
-}
+};
 
 int main()
 {
