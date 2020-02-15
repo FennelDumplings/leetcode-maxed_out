7e7969e07ea9f097a7c68eb92460cea4770c938c
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-02-15 17:26:45 +0800

    20200215

---
+++ prob303_easy_Range-Sum-Query-Immutable.cpp
@@ -0,0 +1,57 @@
+// prob303: Range Sum Query - Immutable
+
+/*
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ */
+
+/*
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ */
+
+/*
+ * Note:
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ */
+
+#include <vector>
+
+using namespace std;
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        if(nums.empty())
+            sums = vector<int>();
+        else
+        {
+            int n = nums.size();
+            sums = vector<int>(n, 0);
+            sums[0] = nums[0];
+            for(int i = 1; i < n; ++i)
+                sums[i] = sums[i - 1] + nums[i];
+        }
+    }
+
+    int sumRange(int i, int j) {
+        // 0 <= i <= j <= n-1
+        if(sums.empty())
+            return 0;
+        else if(i == 0)
+            return sums[j];
+        else
+            return sums[j] - sums[i - 1];
+    }
+private:
+    vector<int> sums;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
