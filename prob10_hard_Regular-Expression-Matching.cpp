a2b30c4c5531ab2d9fa4238cec566df3d3309c6d
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-02-18 23:52:20 +0800

    20200218

--- prob10_hard_Regular-Expression-Matching.cpp
+++ prob10_hard_Regular-Expression-Matching.cpp
@@ -148,7 +148,6 @@ public:
                 if(j > 0 && p[j - 1] == '*')
                 {
                     dp[i][j] = dp[i][j] || dp[i][j - 2];
-                    dp[i][j] = dp[i][j] || dp[i][j - 1];
                     dp[i][j] = dp[i][j] || (i > 0 && dp[i - 1][j] && helper(s, p, i, j - 1));
                 }
             }
@@ -159,5 +158,42 @@ private:
     bool helper(const string& s, const string& p, int i, int j)
     {
         return (s[i - 1] == p[j - 1]) || (p[j - 1] == '.');
+    }
+};
+
+// 状态转移改写, 要把图划清楚才能理解
+class Solution_3 {
+public:
+    bool isMatch(string s, string p) {
+        if(s.empty() && p.empty()) return true;
+        if(!s.empty() && p.empty()) return false;
+
+        int n = s.size(), m = p.size();
+        vector<vector<bool> > dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for(int j = 2; j <= m; j += 2)
+            if(p[j - 1] == '*')
+            {
+                if(p[j - 2] != '*')
+                    dp[0][j] = dp[0][j - 2];
+                else
+                    return false; // 剪枝
+            }
+
+        for(int i = 1; i <= n; ++i)
+            for(int j = 1; j <= m; ++j)
+            {
+                if(p[j - 1] == s[i - 1] || p[j - 1] == '.')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if(p[j - 1] == '*')
+                {
+                    if(p[j - 2] == '*')
+                        return false; // 剪枝
+                    dp[i][j] = dp[i][j - 2];
+                    if(p[j - 2] == s[i - 1] || p[j - 2] == '.')
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+            }
+        return dp[n][m];
     }
 };
