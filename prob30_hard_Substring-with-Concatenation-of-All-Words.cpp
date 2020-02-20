e8f3b609a3c53a7333327df9cb9fd798c9488df8
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-02-21 01:29:19 +0800

    20200220

--- prob30_hard_Substring-with-Concatenation-of-All-Words.cpp
+++ prob30_hard_Substring-with-Concatenation-of-All-Words.cpp
@@ -96,3 +96,158 @@ private:
         }
     }
 };
+
+// 每次移动i时重新建mapping, 不是很好
+// 时间空间都上升明显
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        if(s.empty() || words.empty()) return vector<int>();
+        int n = s.size();
+
+        int words_num = words.size();
+        int pattern_len = words[0].size();
+        if(pattern_len == 0)
+        {
+            vector<int> result(n, 0);
+            for(int i = 0; i < n; ++i)
+                result[i] = i;
+            return result;
+        }
+        int total_match_len = pattern_len * words_num;
+        if(n < total_match_len) return vector<int>();
+
+        // pattern_len > 0, words_num > 0, n >= total_match_len
+        vector<int> result;
+        for(int i = 0; i <= n - total_match_len; ++i)
+        {
+            unordered_map<string, int> mapping;
+            for(int i = 0; i < words_num; ++i)
+            {
+                if(mapping.find(words[i]) == mapping.end())
+                    mapping[words[i]] = 1;
+                else
+                    mapping[words[i]]++;
+            }
+            bool match = true;
+            for(int j = 0; j < words_num; ++j)
+            {
+                if(!check(s, i + j * pattern_len, pattern_len, mapping))
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if(match)
+                result.push_back(i);
+        }
+        return result;
+    }
+
+private:
+    bool check(const string& s, int i, int pattern_len, unordered_map<string, int> &mapping)
+    {
+        string sub = s.substr(i, pattern_len);
+        auto it = mapping.find(sub);
+        if(it != mapping.end() && mapping[sub] > 0)
+        {
+            mapping[sub]--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+// 划窗/双指针+精巧的字符串枚举
+class Solution_2 {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        if(s.empty() || words.empty()) return vector<int>();
+        int n = s.size();
+
+        int words_num = words.size();
+        int pattern_len = words[0].size();
+        if(pattern_len == 0)
+        {
+            vector<int> result(n, 0);
+            for(int i = 0; i < n; ++i)
+                result[i] = i;
+            return result;
+        }
+        int total_match_len = pattern_len * words_num;
+        if(n < total_match_len) return vector<int>();
+
+        // pattern_len > 0, words_num > 0, n >= total_match_len
+        unordered_map<string, int> mapping;
+        for(int i = 0; i < words_num; ++i)
+        {
+            if(mapping.find(words[i]) == mapping.end())
+                mapping[words[i]] = 1;
+            else
+                mapping[words[i]]++;
+        }
+        // 到这里与原始解法都一样
+
+        vector<int> result;
+        unordered_map<string, int> matched;
+        for(int i = 0; i < words_num; ++i)
+            if(matched.find(words[i]) == matched.end())
+                matched[words[i]] = 0;
+        for(int i = 0; i < pattern_len; ++i)
+        {
+            // 切分为每段长为 pattern_len 的序列，共有 pattern_len - 1 种
+            int matched_num = 0;
+            int left = i, right = left + pattern_len - 1;
+            for(int i = 0; i < words_num; ++i)
+                matched[words[i]] = 0;
+            while(right < n && left <= n - total_match_len)
+            {
+                string cur = s.substr(right - pattern_len + 1, pattern_len);
+                if(mapping.find(cur) == mapping.end())
+                {
+                    // cur 不匹配 word 中的词，则此前匹配到的词全都作废
+                    // 复位
+                    matched_num = 0;
+                    for(int i = 0; i < words_num; ++i)
+                        matched[words[i]] = 0;
+                    left = right + 1;
+                    right += pattern_len;
+                }
+                else if(mapping[cur] <= matched[cur])
+                {
+                    // cur 匹配了 words 中的词，但是此前已经匹配到了个数
+                    // 右移left 直到第一个 cur
+                    while(s.substr(left, pattern_len) != cur)
+                    {
+                        matched[s.substr(left, pattern_len)]--;
+                        --matched_num;
+                        left += pattern_len;
+                    }
+                    left += pattern_len;
+                    right += pattern_len;
+                }
+                else
+                {
+                    ++matched_num;
+                    matched[cur]++;
+                    if(matched_num == words_num)
+                    {
+                        result.push_back(left);
+                        // left 往右移动一个词
+                        matched[s.substr(left, pattern_len)]--;
+                        left += pattern_len;
+                        --matched_num;
+                    }
+                    right += pattern_len;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+// 其他解法：字符串哈希,Trie,AC自动机
