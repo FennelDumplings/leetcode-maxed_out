139ebe0b91fc98d856388eb58dac5e3f07dd9210
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-11 22:44:53 +0800

    20200311

--- prob127_medium_Word-Ladder.cpp
+++ prob127_medium_Word-Ladder.cpp
@@ -36,6 +36,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -50,7 +51,7 @@ public:
         vector<vector<int> > g; // 邻接表
         d.assign(n + 1, -1);
         g.assign(n + 1, vector<int>());
-        int ed = -1; // 终点
+        int ed = -1; // 终点, 可能取不到
 
         // 建图过程
         for(int i = 0; i < n; ++i)
@@ -106,4 +107,174 @@ private:
 };
 
 // 不建图，直接通过每步的可行状态 BFS
+class Solution_2 {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(wordList.empty()) return 0;
+        int n = wordList.size();
 
+        vector<int> d; // 到起点的距离
+        d.assign(n + 1, -1);
+        d[n] = 1;
+
+        wordList.push_back(beginWord);
+
+        // BFS
+        queue<int> q;
+        q.push(n); // 起点
+        while(!q.empty())
+        {
+            int u = q.front(); // 当前点
+            q.pop();
+            for(int v = 0; v < n; ++v) // 下一点
+            {
+                if(d[v] == -1) // 未访问过
+                {
+                    if(_check(wordList[u], wordList[v]))
+                    {
+                        if(wordList[v] == endWord)
+                            return d[u] + 1;
+                        d[v] = d[u] + 1;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+private:
+    bool _check(string& s1, string& s2)
+    {
+        int cnt = 0;
+        for(int i = 0; i < (int)s1.size(); ++i)
+            if(s1[i] != s2[i])
+                ++cnt;
+        return cnt == 1;
+    }
+};
+
+
+// 不建图，直接通过每步的可行状态 BFS
+// 双向 BFS
+class Solution_3 {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(wordList.empty()) return 0;
+        int n = wordList.size();
+
+        vector<int> d; // 到起点的距离
+        d.assign(n + 1, 0); // 双向BFS, 节点染色，<0 和 >0 是两种颜色
+
+        wordList.push_back(beginWord);
+
+        // BFS
+        queue<int> q;
+        d[n] = 1;
+        q.push(n); // 起点
+
+        // 双向 BFS -- 赵到终点进队
+        // 若没有点进队，则连接不到终点，退化成单向BFS
+        for(int i = 0; i < n; ++i)
+        {
+            if(wordList[i] == endWord)
+            {
+                d[i] = -1; // 给终点染色
+                q.push(i);
+                break;
+            }
+        }
+
+        while(!q.empty())
+        {
+            int u = q.front(); // 当前点
+            q.pop();
+            for(int v = 0; v <= n; ++v) // 下一点
+            {
+                if(_check(wordList[u], wordList[v]))
+                {
+                    if(d[v] == 0) // 未访问过
+                    {
+                        d[v] = (d[u] > 0 ? d[u] + 1 : d[u] - 1);
+                        q.push(v);
+                    }
+                    else
+                    {
+                        if((d[u] ^ d[v]) < 0) // 异号，颜色不同
+                            return abs(d[u]) + abs(d[v]);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+private:
+    bool _check(string& s1, string& s2)
+    {
+        int cnt = 0;
+        for(int i = 0; i < (int)s1.size(); ++i)
+            if(s1[i] != s2[i])
+                ++cnt;
+        return cnt == 1;
+    }
+};
+
+// 另一思路：枚举边找点
+class Solution_4 {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(wordList.empty()) return 0;
+        int n = wordList.size();
+        int l = beginWord.size();
+        unordered_map<string, int> id;
+        wordList.push_back(beginWord);
+
+        int ed = -1;
+        for(int i = 0; i <=n; ++i)
+        {
+            id[wordList[i]] = i;
+            if(wordList[i] == endWord)
+                ed = i;
+        }
+        if(ed == -1) return 0;
+
+        vector<int> d(n + 1, 0);
+        queue<int> q;
+        d[n] = 1;
+        q.push(n);
+        d[ed] = -1;
+        q.push(ed);
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            string cur = wordList[u];
+            for(int i = 0; i < l; ++i)
+            {
+                char nc = cur[i];
+                for(char c = 'a'; c <= 'z'; ++c)
+                {
+                    if(c == nc) continue;
+                    cur[i] = c;
+                    if(id.count(cur)) // 枚举边找点
+                    {
+                        int v = id[cur];
+                        if(d[v])
+                        {
+                            if((d[u] ^ d[v]) < 0)
+                                return abs(d[u]) + abs(d[v]);
+                        }
+                        else
+                        {
+                            d[v] = (d[u] > 0 ? d[u] + 1 : d[u] - 1);
+                            q.push(v);
+                        }
+                    }
+                }
+                cur[i] = nc;
+            }
+        }
+        return 0;
+    }
+};
