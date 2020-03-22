0372a923c3cc9829c8f44e19feae0e6709516ca5
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-22 21:28:50 +0800

    20200322

--- prob294_medium_Flip-Game-2.cpp
+++ prob294_medium_Flip-Game-2.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 using namespace std;
@@ -55,5 +56,79 @@ private:
         }
         mapping[state] = false;
         return false;
+    }
+};
+
+// SG 定理
+// g(x) = FMN(g(0) ^ g(x - 2))
+// g(0) = 0
+// g(1) = 0
+class Solution_2 {
+public:
+    bool canWin(string s) {
+        int n = s.size();
+        if(n < 2) return false;
+        vector<int> state = _get_state(s);
+        int maxx = 0;
+        for(int x: state) maxx = max(maxx, x);
+        if(maxx < 2) return false;
+        vector<int> g(maxx + 1, 0);
+        vector<int> sub;
+        for(int x = 2; x <= maxx; ++x)
+        {
+            sub.clear();
+            for(int i = 0; i * 2 <= x - 2; ++i)
+            {
+                // (0, n - 2) (1, n - 3), ...
+                // (i, n - 2 - i)
+                sub.push_back(g[i] ^ g[x - 2 - i]);
+            }
+            g[x] = _fmn(sub);
+        }
+        int res = 0;
+        for(int x: state) res ^= g[x];
+        return res > 0;
+    }
+
+private:
+    int _fmn(vector<int>& sub)
+    {
+        int n = sub.size();
+        for(int i = 0; i < n; ++i)
+        {
+            if(sub[i] == -1) continue;
+            int nxt = sub[i];
+            while(nxt < n && sub[nxt] != -1)
+            {
+                int tmp = sub[nxt];
+                sub[nxt] = -1;
+                nxt = tmp;
+            }
+        }
+        for(int i = 0; i < n; ++i)
+            if(sub[i] != -1)
+                return i;
+        return n;
+    }
+
+    vector<int> _get_state(string& s)
+    {
+        int n = s.size();
+        int i = 0;
+        vector<int> result;
+        while(i < n)
+        {
+            while(i < n && s[i] == '-')
+                ++i;
+            if(i == n) break;
+            int j = i;
+            while(j < n && s[j] == '+')
+                ++j;
+            int len = j - i;
+            if(len >= 2)
+                result.push_back(len);
+            i = j;
+        }
+        return result;
     }
 };
