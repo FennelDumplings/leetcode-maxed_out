133cfbdbeaf742194fa2d6109818951fe30df710
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-25 18:35:58 +0800

    20200325

--- prob693_easy_Binary-Number-with-Alternating-Bits.cpp
+++ prob693_easy_Binary-Number-with-Alternating-Bits.cpp
@@ -33,7 +33,7 @@ public:
         int h = high_bit(n);
         for(int i = 0; i <= 31 && (1 << i) < h; ++i)
         {
-            if((n & (1 << i)) == (n & (1 << (i + 1))))
+            if((bool)(n & (1 << i)) == (bool)(n & (1 << (i + 1))))
                 return false;
         }
         return true;
