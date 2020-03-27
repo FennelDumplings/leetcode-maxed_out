062f24afc56394b7bb16ab0b8e4ecd1858f38f43
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-27 13:42:40 +0800

    20200327

--- prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
+++ prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -18,9 +18,11 @@
  */
 
 #include <vector>
+#include <climits>
 
 using namespace std;
 
+// 贪心
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -41,5 +43,26 @@ public:
             i = j;
         }
         return result;
+    }
+};
+
+// DP
+// dp[i][j] := 考虑前i天，第i天手中有股票和无股票时手上的现金
+// 初始化：dp[0][0] = 0, dp[0][1] = INT_MIN (初始手上现金为0)
+// 转移 dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+//      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+class Solution_2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n <= 1) return 0;
+        vector<vector<int> > dp(n + 1, vector<int>(2, 0));
+        dp[0][1] = INT_MIN;
+        for(int i = 1; i <= n; ++i)
+        {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n][0];
     }
 };
