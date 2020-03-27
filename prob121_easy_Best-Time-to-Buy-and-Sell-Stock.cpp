8fbb9fb658a8f822ca99b9fad520639ea54fbd86
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-27 15:52:51 +0800

    20200327

--- prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
+++ prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -63,11 +63,12 @@ public:
         dp[0][1][0] = -prices[0]; // 买入
         for(int i = 1; i < n; ++i)
         {
-            dp[1][0][0] = dp[1 - 1][0][0];
-            dp[i][0][1] = max(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i - 1]);
-            dp[i][1][0] = max(dp[i - 1][1][0], dp[i - 1][0][0] - prices[i - 1]);
+            dp[i][0][0] = dp[i - 1][0][0];
+            dp[i][0][1] = max(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i]);
+            dp[i][1][0] = max(dp[i - 1][1][0], dp[i - 1][0][0] - prices[i]);
         }
         return dp[n - 1][0][1];
+        }
     }
 };
 
