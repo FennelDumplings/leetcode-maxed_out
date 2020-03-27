48a9e12415f84343fe94a215082e3e99b4fb1d8a
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-03-27 14:38:14 +0800

    20200327

--- prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
+++ prob121_easy_Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -49,8 +49,8 @@ public:
 // DP
 // dp[i][j] := 考虑前i天，第i天手中有股票和无股票时手上的现金
 // 初始化：dp[0][0] = 0, dp[0][1] = INT_MIN (初始手上现金为0)
-// 转移 dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-//      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+// 转移 dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1])
+//      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1])
 class Solution_2 {
 public:
     int maxProfit(vector<int>& prices) {
@@ -60,9 +60,28 @@ public:
         dp[0][1] = INT_MIN;
         for(int i = 1; i <= n; ++i)
         {
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
         }
         return dp[n][0];
+    }
+};
+
+// 转换为最大子段和
+class Solution_3 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n <= 1) return 0;
+        vector<int> diff(n - 1);
+        for(int i = 0; i < n - 1; ++i)
+            diff[i] = prices[i + 1] - prices[i];
+        vector<int> dp(n - 1, 0);
+        dp[0] = max(0, diff[0]);
+        for(int i = 1; i < n - 1; ++i)
+            dp[i] = max(0, dp[i - 1] + diff[i]);
+        int result = 0;
+        for(int a: dp) result = max(result, a);
+        return result;
     }
 };
