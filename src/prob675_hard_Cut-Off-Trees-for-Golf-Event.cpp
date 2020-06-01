aee0ea6169e2096e2e7e0d2a0029a6e033cec19c
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-06-01 09:08:16 +0800

    20200601

--- src/prob675_hard_Cut-Off-Trees-for-Golf-Event.cpp
+++ src/prob675_hard_Cut-Off-Trees-for-Golf-Event.cpp
@@ -248,9 +248,187 @@ private:
     }
 };
 
+// 优化：增加将 vector.assign 改为 memset (这次的 memset 的改变与下面的优化6相比收益很小)
+// 804ms
+// 英文版 668ms
+struct Point
+{
+    int pos, val;
+    Point(int pos, int val):pos(pos),val(val){}
+};
+
+struct Cmp
+{
+    bool operator()(const Point& point1, const Point& point2) const
+    {
+        return point1.val < point2.val;
+    }
+};
+
+class Solution {
+public:
+    int cutOffTree(vector<vector<int>>& forest) {
+        if(forest[0][0] == 0) return -1;
+        int n = forest.size(), m = forest[0].size();
+        Cmp cmp;
+        vector<Point> trees;
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+            {
+                if(forest[i][j] > 1)
+                    trees.push_back(Point(i * m + j, forest[i][j]));
+            }
+        sort(trees.begin(), trees.end(), cmp); // O(MNlog(MN))
+
+        // 是否所有的树都能走到 O(MN)
+        // 记录所有能走到的点(一维形式) O(MN)
+        unordered_set<int> reached;
+        bool *visited = new bool[n * m];
+        bfs(forest, reached, visited);
+        for(auto tree: trees)
+            if(reached.find(tree.pos) == reached.end())
+                return -1;
+
+        // 所有树都走得到，结果一定不为 -1
+        int result = 0;
+        int prev = 0;
+        bool *used = new bool[n * m];
+        for(auto tree: trees)
+        {
+            int d = dfs(prev, tree.pos, forest, visited, used);
+            result += d;
+            prev = tree.pos;
+        }
+        return result;
+    }
+
+private:
+    int dx[4] = {0, 0, 1, -1}; // 右，左，下，上
+    int dy[4] = {1, -1, 0, 0};
+    using PII = pair<int, int>;
+
+    int dfs(int pos1, const int pos, const vector<vector<int> >& forest, bool* visited, bool* used)
+    {
+        if(pos1 == pos) return 0;
+        int n = forest.size(), m = forest[0].size();
+        int x = pos / m, y = pos % m;
+
+        int result = manhattan(pos1 / m, pos1 % m, x, y);
+        // st2 始终比 st1 多一个 detours
+        // 因为当前点始终从 st1 取，下一个新点如果要 detour，先放进 st2，不需要 detour 才直接进 st1
+        // st1 空了才会把 st2 倒进 st1
+        vector<int> st1, st2;
+        st1.push_back(pos1);
+        memset(visited, false, sizeof(bool) * n * m);
+        memset(used, false, sizeof(bool) * n * m);
+        used[pos1] = true; // 已经见过的点
+        visited[pos1] = true; // 已经使用的点
+        int detours = 0;
+
+        while(true)
+        {
+            if(st1.empty())
+            {
+                // 不增加 detours 到不了终点
+                swap(st1, st2);
+                for(int pos1: st1) used[pos1] = true;
+                ++detours;
+            }
+
+            int pos1 = st1.back();
+            st1.pop_back();
+            if(pos1 == pos)
+                break;
+
+            int x1 = pos1 / m, y1 = pos1 % m;
+            // 与目标方向一致的下一点进 st1, 与目标方向不一致的下一点进 st2
+            // 新点若与目标方向一致则加入 visited 和 used，直接进入 st1
+            // 新点若与目标方向不一致则只加入 visited，先加入 st2 暂存
+            for(int d: positive_direction(x1, y1, x, y))
+            {
+                // (x1, y1) 朝着 (x, y) 的方向
+                int x2 = x1 + dx[d], y2 = y1 + dy[d];
+                if(x2 < 0 || x2 >= n || y2 < 0 || y2 >= m) continue;
+                if(forest[x2][y2] == 0) continue;
+                int pos2 = x2 * m + y2;
+                if(used[pos2]) continue;
+                visited[pos2] = true;
+                used[pos2] = true;
+                st1.push_back(pos2);
+            }
+            for(int d: negative_direction(x1, y1, x, y))
+            {
+                // (x1, y1) 朝着 (x, y) 相反的方向
+                int x2 = x1 + dx[d], y2 = y1 + dy[d];
+                if(x2 < 0 || x2 >= n || y2 < 0 || y2 >= m) continue;
+                if(forest[x2][y2] == 0) continue;
+                int pos2 = x2 * m + y2;
+                if(visited[pos2]) continue;
+                visited[pos2] = true;
+                st2.push_back(pos2);
+            }
+        }
+
+        return result + detours * 2;
+    }
+
+    vector<int> positive_direction(int x1, int y1, int x, int y)
+    {
+        // (x1, y1) 朝着 (x, y) 的方向
+        vector<int> result;
+        if(x1 > x) result.push_back(3); // 上
+        if(x1 < x) result.push_back(2); // 下
+        if(y1 > y) result.push_back(1); // 左
+        if(y1 < y) result.push_back(0); // 右
+        return result;
+    }
+
+    vector<int> negative_direction(int x1, int y1, int x, int y)
+    {
+        // (x1, y1) 朝着 (x, y) 相反的方向
+        vector<int> result;
+        if(x1 >= x) result.push_back(2); // 上
+        if(x1 <= x) result.push_back(3); // 下
+        if(y1 >= y) result.push_back(0); // 左
+        if(y1 <= y) result.push_back(1); // 右
+        return result;
+    }
+
+    void bfs(const vector<vector<int> >& forest, unordered_set<int>& reached, bool* visited)
+    {
+        int n = forest.size(), m = forest[0].size();
+        memset(visited, false, sizeof(bool) * n * m);
+        visited[0] = true;
+        queue<int> q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int p = q.front();
+            q.pop();
+            reached.insert(p);
+            for(int d = 0; d < 4; ++d)
+            {
+                int x = p / m + dx[d], y = (p % m) + dy[d];
+                if(x < 0 || x >= n || y < 0 || y >= m) continue;
+                if(forest[x][y] == 0) continue;
+                int pos = x * m + y;
+                if(visited[pos]) continue;
+                visited[pos] = true;
+                q.push(pos);
+            }
+        }
+    }
+
+    int manhattan(int x1, int y1, int x2, int y2)
+    {
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+};
+
 
 // ------------------------------------------------
 
+// 原始代码
 // 中文版 TLE
 // 英文版 1712ms 勉强过
 struct Point
@@ -343,14 +521,16 @@ private:
         return -1;
     }
 };
+
+//----------------------------
+
+// https://leetcode-cn.com/problems/cut-off-trees-for-golf-event/solution/c-bfsjiu-ji-you-hua-chang-shu-400ms-shuang-bai-by-/
 // 常数优化
 // 优化点1 用自己的struct代替vector<int>作为queue内单元 效率提升100% queue里套vector<int>效率不是一般的低
 // 优化点2 将2维数组展开到1维 效率提升60%
 // 优化点3 memset代替重拷贝或重分配标记矩阵 效率提升40%
 // 优化点4 标记提前到入队列时 效率提升无穷大 (原来习惯是遍历到在=true)
 // 优化点5 剪枝 提前结束遍历
-
-//----------------------------
 
 // 优化1：将 forest[i][j] 值加进 Point 字段，而不在 cmp 中持有 forest 矩阵
 // 1944ms
@@ -804,3 +984,96 @@ private:
         return -1;
     }
 };
+
+// 优化6：visited.assign 改成 memset(收益极高)
+// 640ms
+// 英文版 496ms
+struct Point
+{
+    int pos, val;
+    Point(int pos, int val):pos(pos),val(val){}
+};
+
+struct Cmp
+{
+    bool operator()(const Point& point1, const Point& point2) const
+    {
+        return point1.val < point2.val;
+    }
+};
+
+class Solution {
+public:
+    int cutOffTree(vector<vector<int>>& forest) {
+        if(forest[0][0] == 0) return -1;
+        int n = forest.size(), m = forest[0].size();
+        Cmp cmp;
+        vector<Point> trees;
+        vector<int> spread(m * n);
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+            {
+                spread[i * m + j] = forest[i][j];
+                if(forest[i][j] > 1)
+                    trees.push_back(Point(i * m + j, spread[i * m + j]));
+            }
+        sort(trees.begin(), trees.end(), cmp);
+
+        int ntree = trees.size();
+        int result = 0;
+        bool *visited = new bool[n * m]; // 动态数组只能开在堆上，因为 m, n 编译时不确定
+        for(int i = 0; i < ntree; ++i)
+        {
+            int d;
+            if(i == 0)
+                d = bfs(Point(0, 0), trees[i].pos, spread, visited, m);
+            else
+                d = bfs(Point(trees[i - 1].pos, 0), trees[i].pos, spread, visited, m);
+            if(d == -1)
+                return -1;
+            result += d;
+        }
+        return result;
+    }
+
+private:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+
+    int bfs(Point p1, int p2_pos, const vector<int>& forest, bool* visited, int m)
+    {
+        if(p1.pos == p2_pos)
+            return 0;
+        int n = forest.size() / m;
+        queue<Point> q;
+        memset(visited, false, sizeof(bool) * n * m);
+        visited[p1.pos] = true;
+        q.push(p1);
+        while(!q.empty())
+        {
+            Point p = q.front();
+            q.pop();
+            for(int d = 0; d < 4; ++d)
+            {
+                int i = p.pos / m, j = p.pos % m;
+                int x = i + dx[d], y = j + dy[d];
+                int pos = x * m + y;
+                if(x < 0 || x >= n || y < 0 || y >= m) continue;
+                if(visited[pos]) continue;
+                if(forest[pos] == 0) continue;
+                if(pos == p2_pos) // 改成一维后，只能放在这里，不能放在前面
+                    return p.val + 1;
+                visited[pos] = true;
+                q.push(Point(pos, p.val + 1));
+            }
+        }
+        return -1;
+    }
+};
+
+
+// 暂未使用办法
+// https://leetcode-cn.com/problems/cut-off-trees-for-golf-event/solution/c-160ms-ti-jie-xi-shuo-chang-shu-you-hua-na-xie-sh/
+// 优化1：快速 BFS(四周填充 0)
+// 优化 2：手写队列
+// 优化 3：间隔 BFS
