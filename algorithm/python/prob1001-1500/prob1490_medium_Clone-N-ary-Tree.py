8fe37d30fd309f8fbf95eb0959df5ad5d5f67847
chengzhaoxi <zhaoxi.cheng@learnable@ai>
2021-09-30 21:12:45 +0800

    2021-09-30

--- algorithm/python/prob1001-1500/prob1490_medium_Clone-N-ary-Tree.py
+++ algorithm/python/prob1001-1500/prob1490_medium_Clone-N-ary-Tree.py
@@ -4,19 +4,19 @@ class Node:
         self.val = val
         self.children = children if children is not None else []
 
-# class Solution:
-#     def cloneTree(self, root: "Node") -> "Node":
-#         if root is None:
-#             return None
-#         return self.dfs(root)
-#
-#     def dfs(self, node: "Node") -> "Node":
-#         new_node = Node(node.val)
-#         n_child = len(node.children)
-#         new_node.children = [None] * n_child
-#         for i, child in enumerate(node.children):
-#             new_node.children[i] = self.dfs(child)
-#         return new_node
+class Solution:
+    def cloneTree(self, root: "Node") -> "Node":
+        if root is None:
+            return None
+        return self.dfs(root)
+
+    def dfs(self, node: "Node") -> "Node":
+        new_node = Node(node.val)
+        n_child = len(node.children)
+        new_node.children = [None] * n_child
+        for i, child in enumerate(node.children):
+            new_node.children[i] = self.dfs(child)
+        return new_node
 
 import copy
 
@@ -26,19 +26,17 @@ import copy
 
 import types
 
-def __deepcopy__(self):
-    def dfs(node: "Node") -> "Node":
-        new_node = Node(node.val)
-        n_child = len(node.children)
-        new_node.children = [None] * n_child
-        for i, child in enumerate(node.children):
-            new_node.children[i] = dfs(child)
-        return new_node
-    if root is None:
-        return None
-    return dfs(self)
+def __deepcopy__(self, memo):
+    if self in memo:
+        return memo.get(self)
+    new_root = Node(copy.deepcopy(self.val, memo)
+                   ,copy.deepcopy(self.children, memo))
+    memo[self] = new_root
+    return new_root
 
 class Solution:
     def cloneTree(self, root: 'Node') -> 'Node':
-        root.__deepcopy__ = types.MethodType(__deepcopy__, self)
+        if root is None:
+            return None
+        root.__deepcopy__ = types.MethodType(__deepcopy__, root)
         return copy.deepcopy(root)
