2f95e7f47a6d8a8e03a88759bf7cd43791c0de27
fenneldumplings <2585811498@qq.com>
2022-02-03 15:27:04 +0800

    2022-02-03

---
+++ algorithm/cpp/prob1501-2000/prob1763_easy_Longest-Nice-Substring.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        int n = s.size();
+        vector<int> flags(26);
+        for(int i = 0; i < n; ++i)
+        {
+            //
+        }
+    }
+};
