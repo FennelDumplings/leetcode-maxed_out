643d3d6490c5f558e390e9ebb221fe9889755ed1
fenneldumplings <2585811498@qq.com>
2022-03-16 21:36:58 +0800

    2022-03-16

--- LCP/04.cpp
+++ LCP/04.cpp
@@ -74,8 +74,9 @@ public:
 class Solution {
 public:
     int domino(int n, int m, vector<vector<int>>& broken) {
+        mask = (1 << m) - 1;
         vector<int> b(n + 1, 0);
-        b[n] = (1 << m) - 1;
+        b[n] = mask;
         for(const vector<int>& broken_point: broken)
         {
             int i = broken_point[0];
@@ -85,27 +86,32 @@ public:
         vector<vector<int>> dp(n + 1, vector<int>(1 << m, -1));
         for(int s = 0; s < (1 << m); ++s)
             dp[n][s] = 0;
-        return solve(0, ~b[0], b, dp);
+        return solve(0, (~b[0]) & mask, b, dp);
     }
+
+    int mask;
 
     int solve(int i, int s, const vector<int>& b, vector<vector<int>>& dp)
     {
-        if(dp[i][s] != 0)
+        if(dp[i][s] != -1)
             return dp[i][s];
         dp[i][s] = 0;
-        for(int t = (s - 1) & s; t != s; t = (t - 1) & s)
+        for(int t = s; ; t = (t - 1) & s)
         {
             if((t & b[i + 1]) != 0)
                 continue;
-            int n1 = check1(t);
-            int n2 = check2(s & (~t));
-            dp[i][s] = max(dp[i][s], n1 + n2 + solve(i + 1, ~(t | b[i + 1]), b, dp));
+            int n1 = check1(t); // 竖着放的个数
+            int n2 = check2(s & (~t)); // 横着放的个数
+            dp[i][s] = max(dp[i][s], n1 + n2 + solve(i + 1, (~(t | b[i + 1])) & mask, b, dp));
+            if(t == 0)
+                break;
         }
         return dp[i][s];
     }
 
     int check2(int x)
     {
+        // x 中长度为 2 的连续 1 的个数
         int n = 0;
         while(x > 0)
         {
@@ -123,6 +129,7 @@ public:
 
     int check1(int x)
     {
+        // x 中 1 的个数
         int n = 0;
         while(x > 0)
         {
